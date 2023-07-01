57a81406c8c1b90cf47c8f2b12189f2429fc9cda
wvsr <email.wasimul@gmail.com>
2023-07-01 20:35:53 +0600

    solve chunk issue

--- src/chunk.ts
+++ src/chunk.ts
@@ -1,9 +1,12 @@
-export default function chunk(arr: any[], chunkCount: number):any[]{
-    const chunkArr:any[] = []
-    let sliceCount = 0
-    for(let i=0; i<arr.length; i++){
-        chunkArr.push(arr.slice(sliceCount, chunkCount))
-        sliceCount = sliceCount + chunkCount
+export default function chunk(arr: any[], chunkCount: number): any[] {
+  const chunkArr: any[] = []
+  let sliceCount = 0
+  for (let i = 0; i < arr.length; i++) {
+    chunkArr.push(arr.slice(sliceCount, sliceCount + chunkCount))
+    sliceCount = sliceCount + chunkCount
+    if (sliceCount >= arr.length) {
+      break
     }
-    return chunkArr
+  }
+  return chunkArr
 }
